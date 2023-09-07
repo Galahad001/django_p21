30b1ecd8e3f4438a5aa5cfb7c0fa3eca3b115305
Galahad001 <z.ucha1234@mail.ru>
2023-09-07 13:20:21 +0300

    com

--- blog/my_blog/views.py
+++ blog/my_blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Topic       # Импорт модели с нужными данными
 
 def index(request):
     """Домашняя страница"""
     return render(request, 'my_blog/index.html')
+
+def topics(request):        # Необходимый параметр request полученный от сервера
+    """Выводит список тем"""
+    topics = Topic.objects.order_by('date')      # Запрос к базе данных  на получение обьектов Topic, отсортированных по полю date
+    context = {'topics': topics}                # Определяется контекст который передается шаблону. Ключ-это имя по которому в шаблонк можно получить значение
+    return render(request, 'my_blog/topics.html', context)
 
 # Create your views here.
